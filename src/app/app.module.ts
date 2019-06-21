278d69d6fd061cf85e5fed780ae85fdc43bed6eb
Michael Gray <michael@mgray.me>
2019-06-20 21:19:19 -0600

    Created Components
    created components, services and interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {DsAppComponent} from './app-ds.component';
+import { DsAppComponent } from './app-ds.component';
+import { UsersComponent } from './users/users.component';
+import { UsersDetailsComponent } from './users-details/users-details.component';
 
 @NgModule({
   declarations: [
-    DsAppComponent
+    DsAppComponent,
+    UsersComponent,
+    UsersDetailsComponent
   ],
   imports: [
     BrowserModule,
